74ee9e00f95c51cf325b6a46ec78628c3c3865a6
Peter Caulcrick <caulcrickp@gmail.com>
2026-01-24 19:40:42 +0100

    Enabled vercel web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/Footer";
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
